7c9b43a3d58aa4b5693fda48b66ba8a846fcdafb
Darshan_R <darshanr123409@gmail.com>
2026-07-22 23:29:06 +0530

    Added Binary Search problems

---
+++ Binary_Search/BS_on_Answer/Koko_Eating_Bananas.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+long long calculateTotalHours(vector<int>& piles, int speed) {
+
+        long long totalH = 0;
+
+        for (int bananas : piles) {
+            totalH += ((long long)bananas + speed - 1) / speed;
+        }
+
+        return totalH;
+}
+
+int minEatingSpeed(vector<int>& piles, int h) {
+
+        int maxPile = *max_element(piles.begin(), piles.end());
+
+        int low = 1;
+        int high = maxPile;
+
+        int ans = maxPile;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            long long totalH = calculateTotalHours(piles, mid);
+
+            if (totalH <= h) {
+                ans = mid;
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+}
